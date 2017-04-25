93adee3e81bff7177e3c638d440e397e14ca6f4e
Daniel <danbanwo@gmail.com>
2017-04-25 18:19:19 -0400

    create component content and remove placeholder

--- front/components/team-selection/Player_List.js
+++ front/components/team-selection/Player_List.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
+import PlayerPreview from './Player_Preview';
+
 class PlayerList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tHead: ['NAME', 'POS', 'FPPG', 'PLAYED', 'GAME', 'SALARY']
+    }
+  }
+
+  displayTHeader = () => {
+    let { tHead } = this.state;
+    return tHead.map((header, idx) => (
+      <th key={idx}>{header}</th>
+    ))
+  }
+
   render() {
     return (
-      <div className='player-list'>
-        Hello from the Player List Component!
+      <div className='player-list' style={{width: '45vw'}}>
+
+        <table className="table table-striped" style={{width: '100%'}}>
+
+          <thead>
+            <tr>
+              {this.displayTHeader()}
+            </tr>
+          </thead>
+
+          {/* The table body contained in <PlayerPreview /> goes below  */}
+          <PlayerPreview />
+
+        </table>
+
       </div>
     )
   }
--- front/components/team-selection/Player_Preview.js
+++ front/components/team-selection/Player_Preview.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 class PlayerPreview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playerStats: [
+        {name: 'John Doe', pos: 'FWD', fppg: '28.5', played: '32', game: 'ARS @ CHE', salary: '$2,000'},
+        {name: 'Jane Doe', pos: 'MID', fppg: '38.5', played: '28', game: 'ARS @ CHE', salary: '$11,000'},
+        {name: 'Jake Doe', pos: 'DEF', fppg: '18.5', played: '7', game: 'ARS @ CHE', salary: '$900'}
+      ]
+    }
+  }
+
+  /* This will display all players associated with current filter option. STATS to be shown are:
+    team-jersey-svg, name, pos, fppg, played, game and salary
+  */
+  displayTBody = () => {
+    let { playerStats } = this.state;
+    return playerStats.map((stat, idx) => (
+      <tr key={idx}>
+        <td key={idx + '-' + stat.name}>{stat.name}</td>
+        <td key={idx + '-' + stat.pos}>{stat.pos}</td>
+        <td key={idx + '-' + stat.fppg}>{stat.fppg}</td>
+        <td key={idx + '-' + stat.played}>{stat.played}</td>
+        <td key={idx + '-' + stat.game}>{stat.game}</td>
+        <td key={idx + '-' + stat.salary}>{stat.salary}</td>
+        <td key={idx + 'btn'}><button className='btn btn-success'>+</button></td>
+      </tr>
+    ))
+  }
+
   render() {
     return (
-      <div className='player-preview'>
-        Hello from the Player Preview Component!
-      </div>
+      <tbody className='player-preview'>
+        {this.displayTBody()}
+      </tbody>
     )
   }
 }
