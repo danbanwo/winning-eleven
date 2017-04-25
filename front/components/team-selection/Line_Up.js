6aecaf082b358ba71615296f8acedfca908fdff1
Daniel <danbanwo@gmail.com>
2017-04-25 19:14:48 -0400

    name change from AddPlayer

---
+++ front/components/team-selection/Line_Up.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LineUp extends Component {
+  render() {
+    return(
+      <div className="add-player">
+        Hello from the Add Player Component!
+      </div>
+    )
+  }
+}
+
+export default LineUp;
