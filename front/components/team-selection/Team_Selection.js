420d0151a477fcbfe04f4b9c5a3296361d4b5e83
Daniel <danbanwo@gmail.com>
2017-04-27 09:06:08 -0400

    build component and add more elements with context

--- front/components/team-selection/Team_Selection.js
+++ front/components/team-selection/Team_Selection.js
@@ -9,28 +9,64 @@ class TeamSelection extends Component {
   render() {
     return (
       <div className='team-selection'>
-        Hello from the Team Selection Page!
+        <section className='team-selection-header' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
 
-        <GameFilter />
+          {/* Title: Name of contest */}
+          <div>
+            <h2>Name of the contest ($11,000 to 1st)</h2>
 
-          <div className='team-selection-body' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+            {/* Contest details */}
+            <div className='team-contest-details' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
 
-            <div className='team-selection-left'>
-
-              <div className='team-filter-search' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                <PlayerFilter />
-                <PlayerSearch />
+              <div className='contest-details'>
+                <h4>Tournament</h4>
+                <h5>contest type</h5>
               </div>
 
+              <div className='contest-details'>
+                <h4>1,234/12,345</h4>
+                <h5>entries</h5>
+              </div>
 
-              <PlayerList />
-            </div>
+              <div className='contest-details'>
+                <h4>$7</h4>
+                <h5>entry fee</h5>
+              </div>
 
-            <div className='team-selection-right'>
-              <LineUp />
+              <div className='contest-details'>
+                <h4>$132,000</h4>
+                <h5>prizes</h5>
+              </div>
             </div>
 
           </div>
+
+          <div className='countdown'><h3>Countdown</h3></div>
+
+        </section>
+
+        <GameFilter />
+
+        <div className='team-selection-body' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+
+          <section className='team-selection-left'>
+
+            <div className='team-filter-search' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+              <PlayerFilter />
+              <PlayerSearch />
+            </div>
+
+
+            <PlayerList />
+          </section>
+
+          <section className='team-selection-right' style={{border: 'solid 1px black', width: '40%', height: 'auto'}}>
+
+            <LineUp />
+
+          </section>
+
+        </div>
 
       </div>
     )
